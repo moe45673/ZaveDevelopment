dc5d189233714ba9dd00c99a9ed3b979539548cc
moe45673 <moshe.tenenbaum@gmail.com>
2016-04-13 19:52:05 -0400

    updating Home PC

---
+++ ZaveModel/Factories/ZDFEntry/DefaultZDFEntry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZaveModel;
+
+namespace ZaveModel.Factories.ZDFEntry
+{
+    public class DefaultZDFEntryFactory : ZDFEntryFactory
+    {
+        public DefaultZDFEntryFactory() : base(){
+           
+        }
+        protected override IZDFEntry createZDFEntry(string name)
+        {
+            
+            
+            if (name == "")
+            {
+                return new ZaveModel.ZDFEntry.ZDFEntry();
+            }
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ZaveService/ZDFEntry/ZDFEntryHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZaveModel.ZDFEntry;
+
+namespace ZaveService.ZDFEntry
+{
+    public class ZDFEntryHandler : IZDFEntryHandler
+    {
+        public ZaveModel.IZDFEntry zdfEntry { get; set; }
+
+        public ZDFEntryHandler(ZaveGlobalSettings.Data_Structures.SelectionData selDat)
+        {
+            zdfEntry = new ZaveModel.ZDFEntry.ZDFEntry()
+        }
+    }
+}
