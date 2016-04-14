379eee9aab8e3a647f85decc7fdb9e919775ac10
moe45673 <moshe.tenenbaum@gmail.com>
2016-04-14 12:38:09 -0400

    Added files to be included in commits

---
+++ ZaveService/Factories/ZDFEntry/ZDFEntryFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaveService.Factories.ZDFEntry
+{
+    public abstract class ZDFEntryFactory
+    {
+        public ZDFEntryFactory()
+        {
+
+        }
+
+        public ZaveService.ZDFEntry.ZDFEntryHandler produceZDFEntry(string type)
+        {
+            return createZDFEntry(type); 
+        }
+
+        protected abstract ZaveService.ZDFEntry.ZDFEntryHandler createZDFEntry(string type);
+        
+    }
+}
---
+++ ZaveService/ZDFEntry/ZDFEntryHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZaveModel.ZDF;
+using ZaveGlobalSettings.Data_Structures;
+
+namespace ZaveService.ZDFEntry
+{
+    public abstract class ZDFEntryHandler
+    {
+        protected SelectionState _modelState;
+        protected IZDF _repository;
+
+        public ZDFEntryHandler(SelectionState modelState, IZDF repository)
+        {
+            _modelState = modelState;
+            _repository = repository;
+        }
+
+        public bool ValidateZDFEntry(ZaveModel.ZDFEntry.IZDFEntry entryToValidate)
+        {
+            if (entryToValidate.Source.DocText.Trim().Length == 0)
+            {
+                _modelState.AddError("Highlighted Text", "Highlighted Text is required");
+            }
+                return _modelState.IsValid;
+            
+        }
+
+        public bool CreateZDFEntry(ZaveModel.ZDFEntry.IZDFEntry entryToCreate)
+        {
+            if (!ValidateZDFEntry(entryToCreate))
+                return false;
+
+            try
+            {
+                _repository.Add(entryToCreate);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        
+    }
+}
