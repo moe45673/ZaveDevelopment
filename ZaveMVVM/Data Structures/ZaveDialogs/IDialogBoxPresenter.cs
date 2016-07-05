63b9367e53c17831a37c10122ccf94d4c9cadf51
Nick Zou <z.nick00@gmail.com>
2016-07-05 13:11:05 -0400

    Added Moe's dialogue stuff.

---
+++ ZaveMVVM/Data Structures/ZaveDialogs/IDialogBoxPresenter.cs
@@ -0,0 +1,14 @@
+﻿using ZaveViewModel.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaveDialogs.Presenters
+{
+	public interface IDialogBoxPresenter<T> where T : IDialogViewModel
+	{
+		void Show(T viewModel);
+	}
+}
