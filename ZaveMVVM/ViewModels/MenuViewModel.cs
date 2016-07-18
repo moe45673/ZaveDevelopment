b9e523d429692e7cd982c7a057daf1758db2881d
moe45673 <moshe.tenenbaum@gmail.com>
2016-07-18 19:39:06 -0400

    Changed "menu" to "Menu"

---
+++ ZaveMVVM/ViewModels/MenuViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace ZaveViewModel.ViewModels
+{
+    public class MenuViewModel : BindableBase
+    {
+    }
+}
