0214d4543db814a938bea04394883507b3312ae5
moe45673 <moshe.tenenbaum@gmail.com>
2016-10-21 15:01:09 -0400

    Added ZDFEntryDetail Region, but crashing on startup

---
+++ ZaveService/ZDFEntry/IZDFEntryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZaveModel.ZDFEntry;
+
+namespace ZaveService.ZDFEntry
+{
+    public interface IZDFEntryService
+    {
+        IZDFEntry getZDFEntry(string id);
+    }
+}
---
+++ ZaveService/ZDFEntry/ZDFEntryService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZaveModel.ZDFEntry;
+
+namespace ZaveService.ZDFEntry
+{
+    public class ZDFEntryService : IZDFEntryService
+    {
+
+        public ZDFEntryService(ZaveModel.ZDFEntry.ZDFEntry entry = default(ZaveModel.ZDFEntry.ZDFEntry))
+        {
+            ActiveZDFEntry = entry;
+        }
+
+        public ZaveModel.ZDFEntry.ZDFEntry ActiveZDFEntry { get; set; }
+
+        public IZDFEntry getZDFEntry(string id)
+        {
+
+            return ZaveModel.ZDF.ZDFSingleton.GetInstance().ListEntries().FirstOrDefault(x => x.ID == Convert.ToInt64(id));
+        }
+    }
+}
