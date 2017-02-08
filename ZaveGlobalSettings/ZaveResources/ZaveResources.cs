40cf4dfe35d87d47faa66001b567a99801d7b33c
moe45673 <moshe.tenenbaum@gmail.com>
2017-02-08 12:06:34 -0500

    Cursor now shows up in color

--- ZaveGlobalSettings/ZaveResources/ZaveResources.cs
+++ ZaveGlobalSettings/ZaveResources/ZaveResources.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ZaveGlobalSettings.ZaveResources
@@ -43,10 +38,13 @@ namespace ZaveGlobalSettings.ZaveResources
 
             //programCursor = Marshal.AllocHGlobal(resourceCursor.marker_cursor2.Length);
             //Marshal.Copy(resourceCursor.marker_cursor2, 0, programCursor, resourceCursor.marker_cursor2.Length);
-            using (MemoryStream ms = new MemoryStream(Properties.Resources.marker_cursor2))
-            {
-                ZaveCursor = new Cursor(ms);
-            }
+            //using (MemoryStream ms = new MemoryStream(Properties.Resources.marker_cursor2))
+            //{
+            //    ZaveCursor = new Cursor(ms);
+            //}
+
+            var cross_i = Properties.Resources.marker_cursor2;
+            ZaveCursor = cross_i.CursorFromArrayManaged(0);
             DefaultCursor = new Cursor(Cursors.IBeam.CopyHandle());
 
 
@@ -85,6 +83,78 @@ namespace ZaveGlobalSettings.ZaveResources
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         private static extern bool SetSystemCursor(IntPtr hCursor, uint id);
 
+
+        
+
+
+    }
+
+    public static class Extensions
+    {
+        [StructLayout(LayoutKind.Explicit, Pack = 1)]
+        struct IconHeader
+        {
+            [FieldOffset(0)]
+            public short reserved;
+            [FieldOffset(2)]
+            public short type;
+            [FieldOffset(4)]
+            public short count;
+        }
+
+        [StructLayout(LayoutKind.Explicit, Pack = 1)]
+        struct IconInfo
+        {
+            [FieldOffset(0)]
+            public byte width;
+            [FieldOffset(1)]
+            public byte height;
+            [FieldOffset(2)]
+            public byte colors;
+            [FieldOffset(3)]
+            public byte reserved;
+            [FieldOffset(4)]
+            public short planes; // ICO file
+            [FieldOffset(6)]
+            public short bpp; // ICO file
+            [FieldOffset(4)]
+            public short hotspot_x; // CUR file
+            [FieldOffset(6)]
+            public short hotspot_y; // CUR file
+            [FieldOffset(8)]
+            public int size;
+            [FieldOffset(12)]
+            public int offset;
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr CreateIconFromResource(IntPtr pbIconBits, int dwResSize, bool fIcon, int dwVer);
+        public static Cursor CursorFromArrayManaged(this byte[] data, int imageIndex)
+        {
+            GCHandle handle = GCHandle.Alloc(data, GCHandleType.Pinned);
+
+            IconHeader iHeader = (IconHeader)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(IconHeader));
+
+            if (imageIndex > iHeader.count - 1)
+            {
+                handle.Free();
+                throw new ArgumentOutOfRangeException("imageIndex");
+            }
+
+            IntPtr iconInfoPtr = handle.AddrOfPinnedObject() + Marshal.SizeOf(typeof(IconHeader)) + imageIndex * Marshal.SizeOf(typeof(IconInfo));
+            IconInfo iInfo = (IconInfo)Marshal.PtrToStructure(iconInfoPtr, typeof(IconInfo));
+
+            handle.Free();
+
+            IntPtr iconImage = Marshal.AllocHGlobal(iInfo.size + 4);
+            Marshal.WriteInt16(iconImage + 0, iInfo.hotspot_x);
+            Marshal.WriteInt16(iconImage + 2, iInfo.hotspot_y);
+            Marshal.Copy(data, iInfo.offset, iconImage + 4, iInfo.size);
+
+            IntPtr hCursor = CreateIconFromResource(iconImage, iInfo.size + 4, false, 0x30000);
+            Marshal.FreeHGlobal(iconImage);
+            return new Cursor(hCursor);
+        }
 
 
 
